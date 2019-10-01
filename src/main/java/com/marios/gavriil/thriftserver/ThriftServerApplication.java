54d59c8825e4021a9f38a15c468135ff118eadcd
Marios <mikopos@gmail.com>
2019-10-01 22:06:48 +0300

    kafka integration attempt 1

--- src/main/java/com/marios/gavriil/thriftserver/ThriftServerApplication.java
+++ src/main/java/com/marios/gavriil/thriftserver/ThriftServerApplication.java
@@ -2,7 +2,6 @@ package com.marios.gavriil.thriftserver;
 
 import com.marios.gavriil.thriftserver.util.ThriftServer;
 import org.apache.thrift.transport.TTransportException;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
