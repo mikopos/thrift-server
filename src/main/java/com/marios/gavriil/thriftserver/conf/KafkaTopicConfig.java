8cab7f60bf34c17d827d364f0f7c811d351f927a
Marios <mikopos@gmail.com>
2019-10-02 00:58:30 +0300

    kafka baeldung tutorial

---
+++ src/main/java/com/marios/gavriil/thriftserver/conf/KafkaTopicConfig.java
@@ -0,0 +1,36 @@
+package com.marios.gavriil.thriftserver.conf;
+
+import com.marios.gavriil.thriftserver.entities.LogsDTO;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic1() {
+        return new NewTopic("logsToKafka", 1, (short) 1);
+    }
+}
--- src/main/java/com/marios/gavriil/thriftserver/services/impl/LogsServiceImpl.java
+++ src/main/java/com/marios/gavriil/thriftserver/services/impl/LogsServiceImpl.java
@@ -6,6 +6,8 @@ import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
