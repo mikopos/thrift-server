a15f866d9dfd5cb47b2db3c40c60f36f781bd5d9
Marios <mikopos@gmail.com>
2019-10-02 22:46:30 +0300

    bean changes to work without new

--- src/main/java/com/marios/gavriil/thriftserver/util/ThriftServer.java
+++ src/main/java/com/marios/gavriil/thriftserver/util/ThriftServer.java
@@ -1,5 +1,6 @@
 package com.marios.gavriil.thriftserver.util;
 
+import com.marios.gavriil.thriftserver.conf.KafkaSender;
 import com.marios.gavriil.thriftserver.services.LogsService;
 import com.marios.gavriil.thriftserver.services.impl.LogsServiceImpl;
 import org.apache.thrift.server.TServer;
@@ -9,13 +10,17 @@ import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ThriftServer {
 
      private Logger logger = LoggerFactory.getLogger(ThriftServer.class);
 
      private TServer server;
 
+     @Bean
     public void start() throws TTransportException {
 
         TServerTransport serverTransport = new TServerSocket(8081);
