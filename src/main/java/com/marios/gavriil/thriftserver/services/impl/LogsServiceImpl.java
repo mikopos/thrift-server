db5a49f9bb1bdf40c48609c7c94ecf1c045948ac
Marios <mikopos@gmail.com>
2019-10-06 22:26:41 +0300

    removed autowired

--- src/main/java/com/marios/gavriil/thriftserver/services/impl/LogsServiceImpl.java
+++ src/main/java/com/marios/gavriil/thriftserver/services/impl/LogsServiceImpl.java
@@ -5,7 +5,6 @@ import com.marios.gavriil.thriftserver.services.LogsService;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
